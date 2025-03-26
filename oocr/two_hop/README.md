9f4065cf72332ec748c50c091814bb244702d09d
ZHZisZZ <asap.zzhou@gmail.com>
2025-03-26 17:25:35 +0800

    add second hop results

--- oocr/two_hop/README.md
+++ oocr/two_hop/README.md
@@ -1,7 +1,7 @@
 [Extractive Structures Learned in Pretraining Enable Generalization on Finetuned Facts](https://arxiv.org/pdf/2412.04614)
 
 ```bash
-# train
+# see 
 sh oocr/two_hop/scripts/train.sh /mnt/lustrenew/mllm_safety-shared/models/huggingface/meta-llama/Meta-Llama-3-8B city_first_hop
 
 ```
--- oocr/two_hop/scripts/dev/test.sh
+++
@@ -1,9 +0,0 @@
-model_name_or_path=$1
-data_config=$2
-template=$3
-
-PYTHONPATH=. srun -p mllm_safety --quotatype=reserved --gres=gpu:1 --cpus-per-task=16 --time=3000 \
-    python oocr/two_hop/src/test.py \
-    --model_name_or_path ${model_name_or_path} \
-    --data_config_path oocr/two_hop/configs/${data_config}.yaml \
-    --template ${template}
